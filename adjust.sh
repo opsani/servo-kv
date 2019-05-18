3305ad4e7c90164682038ec9e9be7d97867c17b9
Johnius <me@johnius.ru>
2019-05-18 01:07:40 -0700

    Initial version

---
+++ adjust.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "progress: 1"
+sleep 1
+echo "progress: 25"
+sleep 1
+echo "progress: 50"
+sleep 1
+echo "progress: 75"
+sleep 1
+echo "progress: 99"
+sleep 1
---
+++ query.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo canary.memory: 2048
+echo canary.cpu: 4
+echo canary.replicas: 1
